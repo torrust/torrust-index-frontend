a9baa2c308d62e33938733ec2e86799facdd5b25
ngthhu <hungnt.code@proton.me>
2024-04-23 16:07:10 +0700

    fix:[#448] use Markdown for CreationDateTab

--- components/torrent/TorrentCreationDateTab.vue
+++ components/torrent/TorrentCreationDateTab.vue
@@ -14,7 +14,7 @@
     <template v-if="!collapsed">
       <div class="flex flex-col w-full h-full p-6 grow bg-base-100 rounded-2xl break-words">
         <template v-if="torrent.creation_date">
-          {{ formattedDateFromTimestamp }}
+          <Markdown :source="formattedDateFromTimestamp" />
         </template>
         <template v-else>
           <span class="italic text-neutral-content">No creation date provided.</span>
