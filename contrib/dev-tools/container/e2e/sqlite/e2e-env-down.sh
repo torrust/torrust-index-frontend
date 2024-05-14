f8f08ec41a66b58110f5f053c9d9d6f9763fa49f
Jose Celano <josecelano@gmail.com>
2024-05-14 16:16:18 +0100

    refactor: [#555] rename tracker env vars
    ```
    TORRUST_TRACKER_BACK_           -> TORRUST_TRACKER_
    TORRUST_TRACKER_DATABASE_DRIVER -> TORRUST_TRACKER_CONFIG_OVERRIDE_DB_DRIVER
    TORRUST_TRACKER_API_ADMIN_TOKEN -> TORRUST_TRACKER_CONFIG_OVERRIDE_HTTP_API__ACCESS_TOKENS__ADMIN
    TORRUST_TRACKER_CONFIG          -> TORRUST_TRACKER_CONFIG_TOML
    TORRUST_TRACKER_PATH_CONFIG     -> TORRUST_TRACKER_CONFIG_TOML_PATH
    ```
    
    To follow changes in: https://github.com/torrust/torrust-tracker/issues/851

--- contrib/dev-tools/container/e2e/sqlite/e2e-env-down.sh
+++ contrib/dev-tools/container/e2e/sqlite/e2e-env-down.sh
@@ -2,5 +2,5 @@
 
 USER_ID=${USER_ID:-1000} \
     TORRUST_INDEX_CONFIG=$(cat ./share/default/config/index.e2e.container.sqlite3.toml) \
-    TORRUST_TRACKER_CONFIG=$(cat ./share/default/config/tracker.e2e.container.sqlite3.toml) \
+    TORRUST_TRACKER_CONFIG_TOML=$(cat ./share/default/config/tracker.e2e.container.sqlite3.toml) \
     docker compose down
