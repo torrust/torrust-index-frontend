c0ab0283a96d797031e7b1724edf65faacfaef84
Mario <mariomnt945@gmail.com>
2024-09-22 17:02:25 +0200

    feat: added cursor pointer

--- components/demo/Banner.vue
+++ components/demo/Banner.vue
@@ -12,7 +12,7 @@
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      class="feather feather-x"
+      class="feather feather-x cursor-pointer"
       @click="closeBanner()"
     ><line x1="18" y1="6" x2="6" y2="18" /><line x1="6" y1="6" x2="18" y2="18" />
     </svg>
